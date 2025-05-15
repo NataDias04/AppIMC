31589b5b113a130160fdf42b0fdd81ec2ae64388
Natan <natadiassantos@gmail.com>
2025-05-14 22:14:13 -0300

    estrutura-base

---
+++ lib/imc.dart
@@ -0,0 +1,27 @@
+class CalculadoraIMC {
+  final double peso;
+  final double altura;
+
+  CalculadoraIMC({required this.peso, required this.altura});
+
+  double calcularIMC() {
+    return peso / (altura * altura);
+  }
+
+  String classificarIMC() {
+    final imc = calcularIMC();
+    if (imc < 18.5) {
+      return 'Abaixo do peso';
+    } else if (imc < 24.9) {
+      return 'Peso normal';
+    } else if (imc < 29.9) {
+      return 'Sobrepeso';
+    } else if (imc < 34.9) {
+      return 'Obesidade grau I';
+    } else if (imc < 39.9) {
+      return 'Obesidade grau II';
+    } else {
+      return 'Obesidade grau III';
+    }
+  }
+}
